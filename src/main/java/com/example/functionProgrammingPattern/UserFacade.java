aeea6a9b01b342b157826b74c98af394be88476b
Santanu <santanu@jotno.com.bd>
2020-03-26 10:18:37 +0600

    First Functional Approach using stream and map

--- src/main/java/com/example/functionProgrammingPattern/UserFacade.java
+++ src/main/java/com/example/functionProgrammingPattern/UserFacade.java
@@ -2,20 +2,16 @@ package com.example.functionProgrammingPattern;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class UserFacade {
 
     private UserRepo userRepo;
 
     public List<UserDto> getAllUsers() {
-        List<User> users = userRepo.findAll();
-        List<UserDto> userDtos = new ArrayList<>();
-
-        for (User user : users) {
-            UserDto userDto = toDto(user);
-            userDtos.add(userDto);
-        }
-        return userDtos;
+        return userRepo.findAll().stream().map(this::toDto).collect(toList());
     }
 
     private UserDto toDto(User user) {
